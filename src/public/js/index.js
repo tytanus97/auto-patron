04d9bce9c77d6991a8e5b64932c0108517ce7409
tytanus97 <tytanus97@gmail.com>
2021-01-11 18:40:50 +0100

    fixed script loaded after content load

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,8 +1,12 @@
-gsap.registerPlugin(CSSRulePlugin);
-const titleSpan = CSSRulePlugin.getRule(".content-main > h1 span:before");
-const timeline = gsap.timeline();
 
- timeline.from(titleSpan,{transform:'scaleX(0)'})
-.to(titleSpan,{duration:1,transform:'scaleX(1)'})
-.to(titleSpan,{duration:0.5,borderRadius:'20%'},'<0.5');
 
+window.addEventListener('load', (event) => {
+   
+   gsap.registerPlugin(CSSRulePlugin);
+   const titleSpan = CSSRulePlugin.getRule(".content-main > h1 span:before");
+   const timeline = gsap.timeline();
+   
+    timeline.from(titleSpan,{transform:'scaleX(0)'})
+   .to(titleSpan,{duration:1,transform:'scaleX(1)'})
+   .to(titleSpan,{duration:0.5,borderRadius:'20%'},'<0.5');
+  });
